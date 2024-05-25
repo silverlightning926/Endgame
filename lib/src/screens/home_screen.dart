fbf93746433c2e061b690b9cfc7fff1d6836b681
Siddharth Rao <siddrao.926@gmail.com>
2024-05-25 16:46:21 -0700

    Added Powered By The Blue Alliance Blurb

--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:endgame/src/components/navigation_elements/rounded_bottom_navigation_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -8,6 +9,21 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        actions: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15),
+            child: Text(
+              "Powered By\nThe Blue Alliance",
+              textAlign: TextAlign.right,
+              style: GoogleFonts.ubuntu(
+                fontSize: 10,
+                fontStyle: FontStyle.italic,
+                fontWeight: FontWeight.w400,
+                color: Colors.purple[100],
+              ),
+            ),
+          ),
+        ],
         shape: const RoundedRectangleBorder(
           borderRadius: BorderRadius.vertical(
             bottom: Radius.circular(15),
