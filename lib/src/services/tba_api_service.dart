8e2be06c86691c2a47454562fbd23321387d9a6b
Siddharth Rao <siddrao.926@gmail.com>
2024-06-04 04:16:56 -0700

    Refactoring API Service

--- lib/src/services/tba_api_service.dart
+++ lib/src/services/tba_api_service.dart
@@ -1,139 +1,80 @@
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:http/http.dart' as http;
 import 'package:endgame/src/serialized/tba/tba_event.dart';
 import 'package:endgame/src/serialized/tba/tba_match.dart';
 import 'package:endgame/src/serialized/tba/tba_status.dart';
 import 'package:endgame/src/serialized/tba/tba_team.dart';
 import 'package:endgame/src/serialized/tba/tba_team_simple.dart';
 import 'package:endgame/src/services/api_secrets.dart';
-import 'package:http/http.dart' as http;
 
 class TBAAPIService {
   static const String _baseUrl = 'https://www.thebluealliance.com/api/v3';
 
-  static Future<TBAStatus> getStatus() async {
-    const String url = '$_baseUrl/status';
+  static Future<T> _getRequest<T>(
+    String url,
+    T Function(dynamic json) fromJson,
+  ) async {
     final http.Response response = await http.get(
       Uri.parse(url),
-      headers: <String, String>{
-        'X-TBA-Auth-Key': ApiSecrets.tbaKey,
-      },
+      headers: <String, String>{'X-TBA-Auth-Key': ApiSecrets.tbaKey},
     );
 
     if (response.statusCode == 200) {
-      return TBAStatus.fromJson(
-        jsonDecode(response.body),
-      );
+      return fromJson(jsonDecode(response.body));
     } else {
-      throw const HttpException('Failed to load status');
+      throw HttpException('Failed to load data: ${response.statusCode}');
     }
+  }
+
+  static Future<TBAStatus> getStatus() async {
+    const String url = '$_baseUrl/status';
+    return _getRequest(url, (json) => TBAStatus.fromJson(json));
   }
 
   static Future<TBATeamSimple> getTeamSimple(String teamKey) async {
     final String url = '$_baseUrl/team/$teamKey/simple';
-    final http.Response response = await http.get(
-      Uri.parse(url),
-      headers: <String, String>{
-        'X-TBA-Auth-Key': ApiSecrets.tbaKey,
-      },
-    );
-
-    if (response.statusCode == 200) {
-      return TBATeamSimple.fromJson(
-        jsonDecode(response.body),
-      );
-    } else {
-      throw const HttpException('Failed to load team');
-    }
+    return _getRequest(url, (json) => TBATeamSimple.fromJson(json));
   }
 
   static Future<TBATeam> getTeam(String teamKey) async {
     final String url = '$_baseUrl/team/$teamKey';
-    final http.Response response = await http.get(
-      Uri.parse(url),
-      headers: <String, String>{
-        'X-TBA-Auth-Key': ApiSecrets.tbaKey,
-      },
-    );
-
-    if (response.statusCode == 200) {
-      return TBATeam.fromJson(
-        jsonDecode(response.body),
-      );
-    } else {
-      throw const HttpException('Failed to load team');
-    }
+    return _getRequest(url, (json) => TBATeam.fromJson(json));
   }
 
   static Future<List<TBAMatch>> getTeamMatchesForYear(
       String teamKey, int year) async {
     final String url = '$_baseUrl/team/$teamKey/matches/$year/simple';
-    final http.Response response = await http.get(
-      Uri.parse(url),
-      headers: <String, String>{
-        'X-TBA-Auth-Key': ApiSecrets.tbaKey,
-      },
-    );
-
-    if (response.statusCode == 200) {
-      final List<dynamic> matches = jsonDecode(response.body);
+    return _getRequest(url, (json) {
+      final List<dynamic> matches = json;
       return matches.map((dynamic match) => TBAMatch.fromJson(match)).toList();
-    } else {
-      throw const HttpException('Failed to load team matches');
-    }
+    });
   }
 
   static Future<List<District>> getDistrictsForYear(int year) async {
     final String url = '$_baseUrl/districts/$year';
-    final http.Response response = await http.get(
-      Uri.parse(url),
-      headers: <String, String>{
-        'X-TBA-Auth-Key': ApiSecrets.tbaKey,
-      },
-    );
-
-    if (response.statusCode == 200) {
-      final List<dynamic> districts = jsonDecode(response.body);
+    return _getRequest(url, (json) {
+      final List<dynamic> districts = json;
       return districts
           .map((dynamic district) => District.fromJson(district))
           .toList();
-    } else {
-      throw const HttpException('Failed to load districts');
-    }
+    });
   }
 
   static Future<List<TBAEvent>> getEventsForYear(int year) async {
     final String url = '$_baseUrl/events/$year';
-    final http.Response response = await http.get(
-      Uri.parse(url),
-      headers: <String, String>{
-        'X-TBA-Auth-Key': ApiSecrets.tbaKey,
-      },
-    );
-
-    if (response.statusCode == 200) {
-      final List<dynamic> events = jsonDecode(response.body);
+    return _getRequest(url, (json) {
+      final List<dynamic> events = json;
       return events.map((dynamic event) => TBAEvent.fromJson(event)).toList();
-    } else {
-      throw const HttpException('Failed to load events');
-    }
+    });
   }
 
   static Future<List<TBATeamSimple>> getTeamsSimple(int pageNum) async {
     final String url = '$_baseUrl/teams/$pageNum/simple';
-    final http.Response response = await http.get(
-      Uri.parse(url),
-      headers: <String, String>{
-        'X-TBA-Auth-Key': ApiSecrets.tbaKey,
-      },
-    );
-
-    if (response.statusCode == 200) {
-      final List<dynamic> teams = jsonDecode(response.body);
+    return _getRequest(url, (json) {
+      final List<dynamic> teams = json;
       return teams.map((dynamic team) => TBATeamSimple.fromJson(team)).toList();
-    } else {
-      throw const HttpException('Failed to load teams');
-    }
+    });
   }
 }
