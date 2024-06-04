a417a9398ee655dd6ec16e06f3c792fadabed25f
Siddharth Rao <siddrao.926@gmail.com>
2024-06-04 03:50:04 -0700

    Removed Unused Import

--- lib/src/season_division_picker.dart
+++ lib/src/season_division_picker.dart
@@ -1,6 +1,5 @@
 import 'package:endgame/src/constants/color_constants.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class SeasonDivisionPicker extends StatefulWidget {
   const SeasonDivisionPicker({
