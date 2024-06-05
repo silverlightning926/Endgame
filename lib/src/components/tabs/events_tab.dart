ea97fb5c768e3475323a11bf5c988bad3d3585d4
Siddharth Rao <siddrao.926@gmail.com>
2024-06-05 04:59:16 -0700

    Refactored Event Tab With Riverpod

--- lib/src/components/tabs/events_tab.dart
+++ lib/src/components/tabs/events_tab.dart
@@ -1,39 +1,19 @@
 import 'package:endgame/src/components/dialog/all_event_dialog.dart';
 import 'package:endgame/src/components/dialog/live_event_dialog.dart';
-import 'package:endgame/src/serialized/tba/tba_event.dart';
 import 'package:flutter/material.dart';
 
 class EventsTab extends StatelessWidget {
-  final int currentYear;
-  final List<District> districts;
-  final List<TBAEvent> events;
-
   const EventsTab({
     super.key,
-    required this.currentYear,
-    required this.districts,
-    required this.events,
   });
-
-  List<TBAEvent> get liveEvents => events.where((e) {
-        final now = DateTime.now();
-        return e.startDate!.isBefore(now) && e.endDate!.isAfter(now);
-      }).toList()
-        ..sort((a, b) => a.startDate!.compareTo(b.startDate!));
 
   @override
   Widget build(BuildContext context) {
     return ListView(
       padding: const EdgeInsets.only(top: 135, left: 7, right: 7, bottom: 10),
-      children: [
-        LiveEventDialog(
-          liveEvents: liveEvents,
-        ),
-        AllEventsDialog(
-          currentYear: currentYear,
-          districts: districts,
-          events: events,
-        ),
+      children: const [
+        LiveEventsDialog(),
+        AllEventsDialog(),
       ],
     );
   }
--- lib/src/season_division_picker.dart
+++ lib/src/season_division_picker.dart
@@ -1,61 +1,39 @@
 import 'package:endgame/src/constants/color_constants.dart';
+import 'package:endgame/src/providers/home_screen_data_providers.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class SeasonDivisionPicker extends StatefulWidget {
-  const SeasonDivisionPicker({
-    super.key,
-    required this.seasonDivision,
-    required this.currentSeasonDivisionIndex,
-    required this.onSeasonDivisionChanged,
-  });
-
-  final List<String> seasonDivision;
-  final int currentSeasonDivisionIndex;
-  final ValueChanged<int> onSeasonDivisionChanged;
-
+class SeasonDivisionPicker extends ConsumerWidget {
+  const SeasonDivisionPicker({super.key});
   @override
-  State<SeasonDivisionPicker> createState() => _SeasonDivisionPickerState();
-}
-
-class _SeasonDivisionPickerState extends State<SeasonDivisionPicker> {
-  int selectedIndex = 0;
-  @override
-  void initState() {
-    selectedIndex = widget.currentSeasonDivisionIndex;
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    // TODO: link year to the year of the selected season
+    final List<String> seasonDivision =
+        ref.watch(getSeasonWeeksProvider(DateTime.now().year)).when(
+              data: (List<String> seasonWeeks) {
+                return seasonWeeks;
+              },
+              loading: () => const [],
+              error: (error, stackTrace) => const [],
+            );
     return SizedBox(
       height: 65,
       child: ListView.builder(
         scrollDirection: Axis.horizontal,
-        itemCount: widget.seasonDivision.length,
+        itemCount: seasonDivision.length,
         itemBuilder: (context, index) {
           return Padding(
             padding: const EdgeInsets.only(bottom: 10, right: 10),
             child: ElevatedButton(
               style: ElevatedButton.styleFrom(
-                backgroundColor: selectedIndex == index
-                    ? ColorConstants.dialogColor
-                    : const Color.fromARGB(50, 50, 35, 219),
-                foregroundColor: selectedIndex == index
-                    ? ColorConstants.dialogTextColor
-                    : const Color.fromARGB(119, 255, 255, 255),
+                backgroundColor: ColorConstants.dialogColor,
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(10),
                 ),
               ),
-              onPressed: () {
-                setState(() {
-                  selectedIndex = index;
-                });
-
-                widget.onSeasonDivisionChanged(index);
-              },
+              onPressed: () {},
               child: Text(
-                widget.seasonDivision[index],
+                seasonDivision[index],
                 style: const TextStyle(
                   fontSize: 14,
                   fontWeight: FontWeight.bold,
