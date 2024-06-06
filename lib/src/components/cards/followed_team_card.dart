181b7f322aeb0b353956e5613df6f3644092dc55
Siddharth Rao <siddrao.926@gmail.com>
2024-06-05 21:17:24 -0700

    Added Some Following Team Logic

--- lib/src/components/cards/followed_team_card.dart
+++ lib/src/components/cards/followed_team_card.dart
@@ -1,15 +1,26 @@
 import 'package:endgame/src/constants/color_constants.dart';
+import 'package:endgame/src/providers/storage_providers.dart';
 import 'package:endgame/src/serialized/tba/tba_team_simple.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:toastification/toastification.dart';
 
-class FollowedTeamCard extends StatelessWidget {
+class FollowedTeamCard extends ConsumerStatefulWidget {
   final TBATeamSimple team;
 
   const FollowedTeamCard({
     super.key,
     required this.team,
   });
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() =>
+      _FollowedTeamCardState();
+}
+
+class _FollowedTeamCardState extends ConsumerState<FollowedTeamCard> {
+  bool isLoading = false;
 
   @override
   Widget build(BuildContext context) {
@@ -29,14 +40,14 @@ class FollowedTeamCard extends StatelessWidget {
             Row(
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                TeamImageReplacement(teamNumber: "${team.teamNumber}"),
+                TeamImageReplacement(teamNumber: "${widget.team.teamNumber}"),
                 const SizedBox(width: 10),
                 Column(
                   mainAxisAlignment: MainAxisAlignment.spaceAround,
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Text(
-                      team.name ?? "",
+                      widget.team.nickname ?? "",
                       style: GoogleFonts.roboto(
                         fontSize: 20,
                         fontWeight: FontWeight.bold,
@@ -44,7 +55,7 @@ class FollowedTeamCard extends StatelessWidget {
                       ),
                     ),
                     Text(
-                      "${team.teamNumber}",
+                      "${widget.team.teamNumber}",
                       style: GoogleFonts.roboto(
                         fontSize: 15,
                         color: ColorConstants.dialogTextColor,
@@ -55,13 +66,56 @@ class FollowedTeamCard extends StatelessWidget {
               ],
             ),
             IconButton(
-              onPressed: () {},
-              icon: const Icon(Icons.star),
+              onPressed: _removeTeam,
+              icon: isLoading
+                  ? const SizedBox(
+                      width: 24,
+                      height: 24,
+                      child: CircularProgressIndicator(strokeWidth: 2),
+                    )
+                  : const Icon(Icons.star),
               color: ColorConstants.followedTeamCardFollowButtomColor,
             ),
           ],
         ),
       ),
+    );
+  }
+
+  Future<void> _removeTeam() async {
+    if (widget.team.key != null && !isLoading) {
+      setState(() {
+        isLoading = true;
+      });
+
+      bool success = await ref
+          .read(removeFollowedTeamKeyProvider(widget.team.key!).future);
+
+      if (mounted) {
+        setState(() {
+          isLoading = false;
+        });
+        _showToast(success);
+      }
+    } else {
+      _showToast(false);
+    }
+  }
+
+  void _showToast(bool success) {
+    final message = success
+        ? "Unfollowed ${widget.team.nickname} - ${widget.team.teamNumber}"
+        : "Failed to unfollow ${widget.team.nickname} - ${widget.team.teamNumber}";
+    toastification.show(
+      title: Text(message),
+      context: context,
+      style: ToastificationStyle.fillColored,
+      dragToClose: true,
+      closeOnClick: true,
+      applyBlurEffect: true,
+      autoCloseDuration: const Duration(seconds: 3),
+      type: success ? ToastificationType.success : ToastificationType.error,
+      icon: success ? const Icon(Icons.check) : const Icon(Icons.error),
     );
   }
 }
