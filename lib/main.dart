62219ca844067aad08bbe3c340099ff5b5ce917c
Siddharth Rao <siddrao.926@gmail.com>
2024-06-06 20:38:32 -0700

    Added WakeLock For Debug

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:endgame/src/app.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:wakelock_plus/wakelock_plus.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kDebugMode) {
+    WakelockPlus.enable();
+  }
   runApp(
     const ProviderScope(
       child: Endgame(),
